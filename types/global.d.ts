60456a739dd2dd6bdbbb77ce1eb199f2f7b7f464
retailret <ditis@bk.ru>
2023-12-18 22:37:30 +0300

    lates

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,1 +1,14 @@
 declare module 'canvas-confetti';
+
+declare namespace GameServer {
+    
+    type Shema = import('@colyseus/schema').Schema;
+	type PlayerSchema = import('./../milkywaystd/app/cardgames/server/src/classes/common/Player').Player;
+	export type Player = Omit<PlayerSchema, keyof Shema>
+	export type AziGameState = import('./../milkywaystd/app/cardgames/server/src/classes/states/Azi/AziGameState').AziGameState;
+}
+
+declare namespace Colyseus {
+	export type Client = import('colyseus.js').Client;
+	export type Room<State> = import('colyseus.js').Room<State>;
+}
